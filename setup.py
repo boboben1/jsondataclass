c61b571cdb3eab4e3dd172b4448af89147e725a3
boboben1 <531071+boboben1@users.noreply.github.com>
2024-06-16 12:55:51 -0400

    update

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='betterjsondataclasses',
+    name='betterjsdc',
     version='1.0.0',
     description='A better interface for transforming dataclasses to and from dictionaries.',
     author='Benjamin Brecher',
